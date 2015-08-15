22fd48b43d15e5ca14e9adf898bbec69b8427d67
aaronra <aaronrandrup@gmail.com>
2015-08-15 18:10:54 +0800

    added mobile config

---
+++ mobile-config.js
@@ -0,0 +1,11 @@
+App.info({
+    name: 'Sagip',
+    description: 'one click rescue',
+    author: 'Cloudstaff',
+    website: 'http://localhost:3000'
+});
+
+
+// Set PhoneGap/Cordova preferences
+App.setPreference('BackgroundColor', '0xff0000ff');
+App.setPreference('HideKeyboardFormAccessoryBar', true);
